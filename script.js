2d675f7f6b5d2408ef5a641609c9c4935ce74369
eoehd1ek <eoehd1ek@naver.com>
2021-07-09 08:28:59 +0900

    Feat: "추가 계산 방식 변경", Rename: "이동 img/ "
    범위 계산 방식 변경
    이미지 파일을 모두 img 폴더로 이동

--- script.js
+++ script.js
@@ -1,61 +1,74 @@
 function calc() {
-    // 타블렛 정보
-    let full_tablet_width = parseFloat(document.getElementById("tablet_right").value);
-    let full_tablet_height = parseFloat(document.getElementById("tablet_bot").value);
-
-    // 직접입력을 사용한 정보
-    let old_width = parseFloat(document.getElementById("old_width").value);
-    let old_height = parseFloat(document.getElementById("old_height").value);
-    let sensitivity = parseFloat(document.getElementById("sensitivity").value);
-    let old_ratio = old_width / old_height;
-    
+    // 전체 타블렛 크기
+    let tabletSizeWidth = parseInt(document.getElementById("tablet_size_right").value);
+    let tabletSizeHeight = parseInt(document.getElementById("tablet_size_bottom").value);
+    // 사용 중인 타블렛 좌표 크기
+    let tabletAreaWidth = parseInt(document.getElementById("tablet_area_width").value);
+    let tabletAreaHeight = parseInt(document.getElementById("tablet_area_height").value);
+    // 현재 해상도와 마우스감도
+    let nowWidth = parseInt(document.getElementById("now_width").value);
+    let nowHeight = parseInt(document.getElementById("now_height").value);
+    let nowSpeed = parseFloat(document.getElementById("now_speed").value);
     // 변경할 해상도
-    let new_width = parseFloat(document.getElementById("new_width").value);
-    let new_height = parseFloat(document.getElementById("new_height").value);
-    let new_ratio = new_width / new_height;
+    let newWidth = parseInt(document.getElementById("new_width").value);
+    let newHeight = parseInt(document.getElementById("new_height").value);
 
 
-    // 타블렛 정보에 감도값 적용
-    let tablet_width = full_tablet_width / sensitivity;
-    let tablet_height = full_tablet_height / sensitivity;
+    // 배율(speed) 적용
+    let resultWidth = tabletAreaWidth / nowSpeed;
+    let resultHeight = tabletAreaHeight / nowSpeed;
+    // 가로 길이 보정
+    let resolutionWidth = (newWidth / nowWidth) / (newHeight / nowHeight)
+    /* 옛 해상도와 타블렛 크기를 가로보정 길이만큼 늘리면
+     * 옛 해상도와 목표 해상도의 비율이 같아짐.
+     * 이후 (목표 해상도 / 옛 해상도)의 비율 만큼 타블렛 크기를 나눔 */
+    nowWidth *= resolutionWidth;
+    resultWidth *= resolutionWidth;
+    resultWidth /= (newWidth / nowWidth);
+    resultHeight /= (newHeight / nowHeight);
+    console.log(`width: ${resultWidth}\nheight: ${resultHeight}`);
 
-
-    // 해상도 차이 적용
-    let outside = old_width * new_height;
-    let inside = old_height * new_width;
-
-    let minimal = outside;
-    let maximal = inside;
-    if(minimal > maximal) {
-        let tmp = minimal;
-        minimal = maximal;
-        maximal = tmp;
-    }
-
-    let bias = maximal / minimal;
-    console.log(bias);
-    // result에 총 가로, 세로 길이 정보 저장
-    let result_width = 0;
-    let result_height = tablet_height;
-    // 예전이 큰 비율, 현재가 작은 비율     (ex 16:9 -> 5:4)
-    if (old_ratio > new_ratio) {
-        result_width = tablet_width / bias;
-    } // 예전이 작은 비율, 현재가 큰 비율   (ex 5:4 -> 16:9)
-    else {
-        result_width = tablet_width * bias;
-    }
-    
-    console.log(result_width);
-    console.log(result_height);
-
-    let ans_top = (full_tablet_height - result_height) / 2;
-    let ans_bot = full_tablet_height - ans_top;
-    let ans_left = (full_tablet_width - result_width) / 2;
-    let ans_right = full_tablet_width - ans_left;
+    let resultTop = (tabletSizeHeight - resultHeight) / 2;
+    let resultBottom = tabletSizeHeight - resultTop;
+    let resultLeft = (tabletSizeWidth - resultWidth) / 2;
+    let resultRight = tabletSizeWidth - resultLeft;
 
     // 결과 적용
-    document.getElementById("ans_top").value = ans_top;
-    document.getElementById("ans_bot").value = ans_bot;
-    document.getElementById("ans_left").value = ans_left;
-    document.getElementById("ans_right").value = ans_right;
+    document.getElementById("result_top").value = Math.floor(resultTop);
+    document.getElementById("result_bottom").value = Math.floor(resultBottom);
+    document.getElementById("result_left").value = Math.floor(resultLeft);
+    document.getElementById("result_rigth").value = Math.floor(resultRight);
 }
+
+function calcAreaWidth() {
+    const left = parseInt(elementTabletAreaLeft.value);
+    const right = parseInt(elementTabletAreaRight.value);
+    // console.log(`l: ${left}, r: ${right}`);
+    if ((left != NaN) && (right != NaN)) {
+        elementTabletAreaWidth.value = right - left;
+    }
+}
+
+function calcAreaHeight() {
+    const top = parseInt(elementTabletAreaTop.value);
+    const bottom = parseInt(elementTabletAreaBottom.value);
+    // console.log(`top: ${top}, bot: ${bottom}`);
+    if ((top != NaN) && (bottom != NaN)) {
+        elementTabletAreaHeight.value = bottom - top;
+    }
+}
+
+// 사용 중인 타블렛 좌표 Width, Height 자동 계산
+const elementTabletAreaTop = document.getElementById("tablet_area_top");
+const elementTabletAreaBottom = document.getElementById("tablet_area_bottom");
+const elementTabletAreaLeft = document.getElementById("tablet_area_left");
+const elementTabletAreaRight = document.getElementById("tablet_area_right");
+const elementTabletAreaWidth = document.getElementById("tablet_area_width");
+const elementTabletAreaHeight = document.getElementById("tablet_area_height");
+
+// 가로
+elementTabletAreaLeft.addEventListener("input", calcAreaWidth);
+elementTabletAreaRight.addEventListener("input", calcAreaWidth);
+// 세로
+elementTabletAreaTop.addEventListener("input", calcAreaHeight);
+elementTabletAreaBottom.addEventListener("input", calcAreaHeight);
